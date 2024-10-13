03086ab0db1c0f6918d857801fb767630d87bc09
Daniel <daniel.sofran@stud.ubbcluj.ro>
2024-10-13 23:54:20 +0300

    Changed Android icon, splash, name. Still in progress

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.ubb.unofficial.devs',
   appName: 'UBB Orar FMI',
-  webDir: 'dist'
+  webDir: 'dist',
 };
 
 export default config;
