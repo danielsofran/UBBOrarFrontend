973d85ddbde51b8f8bf954d6d17e7273bc217bbd
Daniel <daniel.sofran@stud.ubbcluj.ro>
2024-10-22 22:24:51 +0300

    Updates

--- assets/ios.transition-legacy-Cg0qBN3s.js
+++ assets/ios.transition-legacy-Cg0qBN3s.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CGu5b7fj.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
+System.register(["./index-legacy-D5vOkTKj.js"],(function(t,e){"use strict";var o,n;return{setters:[t=>{o=t.n,n=t.o}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
--- assets/swipe-back-legacy-CeBpQIYr.js
+++ assets/swipe-back-legacy-CeBpQIYr.js
@@ -1,4 +1,4 @@
-System.register(["./index-legacy-CGu5b7fj.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
+System.register(["./index-legacy-D5vOkTKj.js"],(function(t,e){"use strict";var n,r,i;return{setters:[t=>{n=t.i,r=t.c,i=t.a}],execute:function(){
 /*!
              * (C) Ionic http://ionicframework.com - MIT License
              */
